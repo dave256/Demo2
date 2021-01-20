5707d1e1f294f9062a7d2988baa2076de6b86c89
Dave Reed <dave256@mac.com>
2021-01-20 17:29:31 -0500

    version4

--- Demo2/ContentView.swift
+++ Demo2/ContentView.swift
@@ -7,32 +7,36 @@
 
 import SwiftUI
 
-struct PressCount {
-    var count1 = 0
-    var count2 = 0
+
+struct ButtonCount: View {
+    let buttonName: String
+    @Binding var pressCount: Int
+
+    var body: some View {
+        VStack {
+            Text("\(buttonName) \(pressCount) times")
+            Button {
+                pressCount += 1
+            } label: {
+                Text(buttonName)
+            }
+            .padding([.top, .bottom], 8)
+        }
+    }
 }
 
 struct ContentView: View {
     // State should always be private
-    @State private var pressCounts = PressCount()
+    @State private var pressCounts = Array(repeating: 0, count: 3)
+
+    var totalPresses: Int { pressCounts.reduce(0, +) }
 
     var body: some View {
         VStack {
-            Text("Button 1 pressed \(pressCounts.count1) times")
-            Button {
-                pressCounts.count1 += 1
-            } label: {
-                Text("Button 1")
-            }
-            .padding([.top, .bottom], 8)
-
-            Text("Button 2 pressed \(pressCounts.count2) times")
-            Button {
-                pressCounts.count2 += 1
-            } label: {
-                Text("Button 2")
-            }
-            .padding([.top, .bottom], 8)
+            ButtonCount(buttonName: "Button1", pressCount: $pressCounts[0])
+            ButtonCount(buttonName: "Button2", pressCount: $pressCounts[1])
+            ButtonCount(buttonName: "Button3", pressCount: $pressCounts[2])
+            Text("Total Counts \(totalPresses)")
         }
     }
 }
