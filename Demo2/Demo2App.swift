0687d506430ae6198762cbefa0235a74980cef2e
Dave Reed <dave256@mac.com>
2021-01-20 17:06:23 -0500

    Initial Commit

---
+++ Demo2/Demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo2App.swift
+//  Demo2
+//
+//  Created by David M Reed on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
